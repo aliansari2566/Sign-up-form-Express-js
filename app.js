da419272c6080e09072afb9c43ff00d47744e80b
Muhammad Ali Hamza <71075942+aliansari2566@users.noreply.github.com>
2023-10-03 19:00:11 +0500

    new changes

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const axios = require("axios");
+const request = require("request");
 const bodyParser = require("body-parser");
 
 const app = express();
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signUp.html");
+  });
 
 app.listen(3000, function () {
     console.log("server is running on port 3000");
